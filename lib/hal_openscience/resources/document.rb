c661ce6052ace99175c1d28a9ec090d2bc5c3c22
Arnaud Levy <contact@arnaudlevy.com>
2026-01-27 17:38:11 +0100

    types

--- lib/hal_openscience/resources/document.rb
+++ lib/hal_openscience/resources/document.rb
@@ -1,5 +1,82 @@
 module HalOpenscience
   class Document < BaseResource
+    TYPES = [
+      'ART',
+      'COMM',
+      'COUV',
+      'THESE',
+      'OUV',
+      'MEM',
+      'UNDEFINED',
+      'POSTER',
+      'REPORT',
+      'OTHER',
+      'IMG',
+      'ISSUE',
+      'PROCEEDINGS',
+      'NOTICE',
+      'BLOG',
+      'HDR',
+      'PATENT',
+      'VIDEO',
+      'REPORT_LABO',
+      'LECTURE',
+      'TRAD',
+      'REPORT_MAST',
+      'REPORT_LPRO',
+      'REPORT_LICE',
+      'SOFTWARE',
+      'PRESCONF',
+      'CREPORT',
+      'SON',
+      'REPORT_DOCT',
+      'MAP',
+      'REPORT_ETAB',
+      'REPORT_FORM',
+      'REPORT_GMAST',
+      'NOTE',
+      'SYNTHESE',
+      'REPORT_FPROJ',
+      'REPORT_GLICE',
+      'ETABTHESE',
+      'REPACT',
+      'MEMLIC',
+      'REPORT_RFOINT',
+      'REPORT_COOR',
+      'REPORT_RETABINT',
+      'OTHERREPORT'
+    ].freeze
+
+    SUBTYPES = [
+      'PHOTOGRAPHY',
+      'RESREPORT',
+      'BOOKREVIEW',
+      'ARTREV',
+      'TECHREPORT',
+      'ILLUSTRATION',
+      'DATAPAPER',
+      'WORKINGPAPER',
+      'PREPRINT',
+      'FUNDREPORT',
+      'GRAPHICS',
+      'EXPERTREPORT',
+      'SYNTOUV',
+      'CRIT',
+      'MANUAL',
+      'DRAWING',
+      'GRAVURE',
+      'DICTIONAR',
+      'DMP'
+    ].freeze
+
+    SUBTYPES_PER_TYPE = {
+      'ART' => ['ARTREV', 'DATAPAPER', 'BOOKREVIEW'],
+      'IMG' => ['DRAWING', 'GRAVURE', 'ILLUSTRATION', 'GRAPHICS', 'PHOTOGRAPHY'],
+      'OUV' => ['SYNTOUV', 'MANUAL', 'DICTIONAR', 'CRIT'],
+      'UNDEFINED' => ['PREPRINT', 'WORKINGPAPER'],
+      'REPORT' => ['RESREPORT', 'TECHREPORT', 'FUNDREPORT', 'EXPERTREPORT', 'DMP']
+    }
+
     def self.repository_url
       "#{HalOpenscience::BASE_URL}/search/"
     end
