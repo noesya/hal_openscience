ef3ac56f4e127479081ec76a3684114dc67b3d14
Arnaud Levy <contact@arnaudlevy.com>
2026-01-28 06:38:51 +0100

    load locale

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,7 @@
 - Initial release with basic features
   - Author search
   - Document search (by term or person ID)
+
+## [1.0.0] - 2025-01-28
+
+- Add doctypes
--- lib/hal_openscience.rb
+++ lib/hal_openscience.rb
@@ -5,9 +5,9 @@ require "net/http"
 require "open-uri"
 
 require "hal_openscience/version"
-
 require "hal_openscience/response"
 require "hal_openscience/resources"
+require "hal_openscience/railtie" if defined?(Rails::Railtie)
 
 module HalOpenscience
   BASE_URL = "https://api.archives-ouvertes.fr"
